d51c53e209f3bbe8dbdbed6469ee20164affc8a9
Tom Arra <tarra3@gmail.com>
2026-06-30 18:29:35 -0500

    fix: make health dependencies explicit

--- api/src/modules/health/health.controller.ts
+++ api/src/modules/health/health.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Inject } from "@nestjs/common";
 import { ApiOkResponse, ApiTags } from "@nestjs/swagger";
 
 import { HealthService } from "./health.service.js";
@@ -6,7 +6,7 @@ import { HealthService } from "./health.service.js";
 @ApiTags("health")
 @Controller("health")
 export class HealthController {
-  constructor(private readonly healthService: HealthService) {}
+  constructor(@Inject(HealthService) private readonly healthService: HealthService) {}
 
   @Get()
   @ApiOkResponse({ description: "API process health check." })
